d33faf074745c5bcc655881c515912c2f483faf4
Princeton <princeton@Thilakshans-MacBook-Air.local>
2020-07-06 16:05:33 -0400

    added state to the health bar

--- src/stories/HealthBar.stories.js
+++ src/stories/HealthBar.stories.js
@@ -4,7 +4,4 @@ import { storiesOf } from "@storybook/react";
 import HealthBar from '../components/HealthBar';
 
 storiesOf("HealthBar", module)
-  .add("3 full hearts", () => <HealthBar health={3}></HealthBar>)
-  .add("2 full hearts + 1 empty heart", () => <HealthBar health={2}></HealthBar>)
-  .add("1 full heart + 2 empty hearts", () => <HealthBar health={1}></HealthBar>)
-  .add("0 full hearts + 3 empty hearts", () => <HealthBar health={0}></HealthBar>)
+  .add("Health Bar", () => <HealthBar health={3}></HealthBar>)
