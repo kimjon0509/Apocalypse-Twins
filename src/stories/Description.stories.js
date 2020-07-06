c360aab717d913f2225710ffeebad1291c63cfb5
John Kim <kimjon0509@gmail.com>
2020-07-06 11:06:53 -0400

    typewriter effect for the scene - still need to modify speed

---
+++ src/stories/Description.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf } from "@storybook/react";
+// import { action } from "@storybook/addon-actions";
+import Description from '../components/Description';
+
+const sampleTextArr = 'It’s been 4 years since the dead overtook the streets. You and your twin have managed to survive in an old, dilapidated apartment building. The two of you have… unusual abilities, which have undoubtedly helped keep you alive.'
+
+storiesOf("Description", module)
+  .add("Description", () => <Description text={sampleTextArr} maxLen={60}></Description>)
